8afdf0bde3e84148d9dde76f1b13b5a506797ae5
Noice75 <matrriixx69@gmail.com>
2024-08-16 20:22:54 +0530

    login with server authority

--- docs/utils/checkAuth.js
+++
@@ -1,3 +0,0 @@
-import supabase from "./initialize.js";
-const { data: { session } = {} } = await supabase.auth.getSession(); //Gets Saved User Session
-if (!session) window.location.href = "login.html"; // If already loggedin Redirect to home page
