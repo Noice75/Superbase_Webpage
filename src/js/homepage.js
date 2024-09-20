bfe793dec938f0477f7d88687ffda5b9cc1d05da
Noice75 <matrriixx69@gmail.com>
2024-09-20 14:59:18 +0530

    update

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,8 +1,14 @@
 var profileButton = document.getElementById("profileButton");
 var Gossip = document.getElementById("Gossip");
+var Chat = document.getElementById("Chat");
 profileButton.addEventListener("click", function () {
   window.location.href = "./profile.html";
 });
+
 Gossip.addEventListener("click", function () {
   window.location.href = "./gossip.html";
+});
+
+Gossip.addEventListener("click", function () {
+  window.location.href = "./livechat.html";
 });
